11050e6ae1ff0000459a737fe8b519f44d37fff3
Daw588 <44325281+Daw588@users.noreply.github.com>
2022-07-08 00:02:35 -0500

    Added MessagingService API
    Added a new method "PublishMessageAsync" from MessagingService API which is equivalent to "PublishAsync".

--- src/modules/universe.ts
+++ src/modules/universe.ts
@@ -5,12 +5,20 @@ import type * as DataStoreRequestHeaders from "../types/endpoints/datastore/requ
 import type * as DataStoreQueryParams from "../types/endpoints/datastore/query-params.js";
 import type * as DataStoreResponseJSON from "../types/endpoints/datastore/response-json.js";
 
+import type * as MessagingServiceRequestHeaders from "../types/endpoints/messaging-service/request-headers.js";
+
 // Modules
 import * as utils from "../utils.js";
 import { Pages } from "./pages.js";
 
 // Config
-import { DATASTORE_API_BASE_ENDPOINT } from "../config/urls.js";
+import {
+	DATASTORE_API_BASE_ENDPOINT,
+	MESSAGING_SERVICE_API_BASE_ENDPOINT
+} from "../config/urls.js";
+
+// Libraries
+import fetch from "node-fetch";
 
 /**
  * Universe is a collection of places, often referred
@@ -52,6 +60,57 @@ export class Universe {
 			"x-api-key": this.apiKey
 		};
 
-		return new Pages<DataStoreQueryParams.ListDataStores, DataStoreRequestHeaders.ListDataStores, DataStoreResponseJSON.DataStoreInfo>("datastores", rawUrl, queryParams, headers);
+		return new Pages<DataStoreQueryParams.ListDataStores,
+			DataStoreRequestHeaders.ListDataStores,
+			DataStoreResponseJSON.DataStoreInfo>("datastores", rawUrl, queryParams, headers);
+	}
+
+	/**
+	 * This function sends the provided message to all subscribers to the topic,
+	 * triggering their registered callbacks to be invoked.
+	 * 
+	 * Same as MessagingService:PublishAsync()
+	 * @param topic Determines where the message is sent.
+	 * @param message The data to include in the message.
+	 */
+	async PublishMessageAsync(topic: string, message: string) {
+		utils.mustBeString("Topic", topic);
+		utils.mustBeString("Message", message);
+
+		// Disallow the use of non-alphanumeric characters in a topic name (as written in API restrictions)
+		if (!topic.match(new RegExp("^[a-zA-Z0-9]*$"))) {
+			throw Error("Topic name cannot contain non-alphanumeric characters");
+		}
+
+		// Disallow topic name if it contains more than 80 characters (as written in API restrictions)
+		if (topic.length > 80) {
+			throw Error("Topic name cannot contain more than 80 characters");
+		}
+
+		const url = `${MESSAGING_SERVICE_API_BASE_ENDPOINT}/${this.id}/topics/${topic}`;
+
+		const valueJSON = JSON.stringify({
+			message: message
+		});
+
+		const headers: MessagingServiceRequestHeaders.PublishAsync = {
+			"x-api-key": this.apiKey,
+			"Content-Type": "application/json"
+		};
+
+		const response = await fetch(url, {
+			method: "post",
+			headers: headers,
+			body: valueJSON
+		});
+
+		if (response.status !== 200) {
+			return Promise.reject({
+				status: response.status,
+				statusText: response.statusText
+			});
+		}
+
+		return await response.text(); // If successful, it will return empty response body
 	}
 }
---
+++ src/types/endpoints/messaging-service/request-headers.ts
@@ -0,0 +1,8 @@
+import type {
+	XApiKey,
+	ContentType
+} from "./headers.js";
+
+export type PublishAsync = 
+	XApiKey &
+	ContentType;
